90c18700180c63715efafe8c8507fc7c1d8134b2
Luis Quiñones <lpaandres2020@gmail.com>
2024-10-05 18:15:40 -0600

    feat(build): specifying `pub const pkg` with `std.build.Pkg`

--- build.zig
+++ build.zig
@@ -1,13 +1,24 @@
 const std = @import("std");
 
+pub const pkg = std.build.Pkg{
+    .name = "extras",
+    .path = .{ .path = "src/lib.zig" },
+};
+
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
     const mode = b.option(std.builtin.Mode, "mode", "") orelse .Debug;
 
     _ = b.addModule(
         "extras",
-        .{ .root_source_file = b.path("src/lib.zig") },
+        .{ .root_source_file = b.path("src/lib.zig"), .target = target, .optimize = optimize },
     );
+
+    const lib = b.addStaticLibrary(.{ .name = "extras", .root_source_file = b.path("src/lib.zig"), .target = target, .optimize = optimize });
+
+    b.installArtifact(lib);
 
     const exe_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/lib.zig"),
